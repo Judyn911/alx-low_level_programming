fde9cc4cf5c3ad418f96012a2df6bb560cc36058
Judyn911 <romanusjudyn@gmail.com>
2022-09-15 15:19:46 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include “main.h”
+#include <ctype.h>
+
+/**
+ * _isupper-Entry point: prints uppercase
+ * @c: single user input
+ * Return: return 1 if c is uppercase, return 0 if not
+ *
+ *
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
