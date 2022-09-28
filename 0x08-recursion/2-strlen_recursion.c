771318a83c3316d75d49c706c544f9e1a31eaa63
Judyn911 <romanusjudyn@gmail.com>
2022-09-28 13:48:01 -0700

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,37 +1,17 @@
-
-tpauldike
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x08-recursion/2-strlen_recursion.c
-@tpauldike
-tpauldike task 2
- 1 contributor
-20 lines (17 sloc)  281 Bytes
 #include "main.h"
-
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
+ * _strlen_recursion - length of string
+ * @s: string to count
+ * Return: length of string
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	int n = 0;
 
 	if (*s)
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		n = _strlen_recursion(s + 1);
+		return (n += 1);
 	}
-
-	return (len);
+	return (0);
 }
